eff795e05440ee5b07c919e610f52c8c8ab5e453
Cynthia <cyn2993@gmail.com>
2023-08-24 23:34:25 +0200

    Add functionality in select tag

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
-const addTaskIcon = document.getElementById("add-img")
-const textTitle = document.getElementById("input-title")
-const textDescription = document.getElementById("input-description")
-const inputId = document.getElementById("input-id")
-const taskList = document.getElementById("task-list")
+const addTaskIcon = document.getElementById("add-img");
+const textTitle = document.getElementById("input-title");
+const textDescription = document.getElementById("input-description");
+const inputId = document.getElementById("input-id");
+const taskList = document.getElementById("task-list");
+const filterSelect = document.getElementById("filter-selected");
+const sortButton = document.getElementById("sort-button");
 
 async function showTasksInScreen() {
     let result = await fetch("http://localhost:3000/tasks");
@@ -20,11 +22,25 @@ async function showTasksInScreen() {
 
 showTasksInScreen();
 
-let showAddedTasks = addTaskIcon.addEventListener("click", async () => {
+addTaskIcon.addEventListener("click", async () => {
     let inputTitle = textTitle.value;
     let inputDescription = textDescription.value;
-    const date = new Date();
-    const textDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    const currentDate = new Date();
+
+    // Crear una nueva fecha sumando 15 días a la fecha actual
+    const dueDate = new Date(currentDate);
+    dueDate.setDate(currentDate.getDate() + 15);
+    const textDate = `${currentDate.getFullYear()}-${(
+        currentDate.getMonth() + 1
+    )
+        .toString()
+        .padStart(2, "0")}-${currentDate
+        .getDate()
+        .toString()
+        .padStart(2, "0")}`;
+    const dueDateString = `${dueDate.getFullYear()}-${(dueDate.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${dueDate.getDate().toString().padStart(2, "0")}`;
     if (inputTitle && inputDescription !== "") {
         let result = await fetch("http://localhost:3000/tasks", {
             method: "POST",
@@ -35,24 +51,74 @@ let showAddedTasks = addTaskIcon.addEventListener("click", async () => {
                 title: inputTitle,
                 description: inputDescription,
                 createdAt: textDate,
-                completed: false, 
+                dueDate: dueDateString,
+                completed: false,
             }),
         });
-        
-        let data = await result.json(); 
+        let data = await result.json();
         let taskItem = document.createElement("li");
         taskItem.classList.add("task-item");
         taskItem.innerHTML = `<input type="checkbox" onclick="postCheckedTask(${data.id}, this)" id="${data.id}"> <strong>${data.title}</strong> ${data.description} <img src="../assets/edit.png" class="edit-image" onclick="editTask(${data.id})" id="${data.id}"/> <img src="../assets/garbage.png" class="delete-image" onclick="deleteTask(${data.id})" id="${data.id}"/>`;
         taskList.appendChild(taskItem);
     } else {
         Swal.fire({
-            title: 'Complete your task!',
-            imageUrl: '../../src/assets/meme-patrick-star.gif',
+            title: "Complete your task!",
+            imageUrl: "../../src/assets/meme-patrick-star.gif",
             imageWidth: 400,
             imageHeight: 200,
-            imageAlt: 'Gif image in sweet alert',
+            imageAlt: "Gif image in sweet alert",
         });
     }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sortButton = document.getElementById("sort-button");
+    const filterSelect = document.getElementById("filter-selected");
+    const taskList = document.getElementById("task-list");
+
+    sortButton.addEventListener("click", async (event) => {
+        event.preventDefault();
+        const selectedFilter = filterSelect.value;
+        console.log(selectedFilter);
+        let result = await fetch("http://localhost:3000/tasks");
+        let data = await result.json();
+
+        switch (selectedFilter) {
+            case "createdAt":
+                data.sort(
+                    (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+                );
+                break;
+            case "dueDate":
+                data.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+                break;
+            case "completed":
+                data = data.filter((task) => task.completed);
+                break;
+            case "pending":
+                data = data.filter((task) => !task.completed);
+                break;
+            default:
+                break;
+        }
+
+        taskList.innerHTML = "";
+
+        data.forEach((task) => {
+            let taskItem = document.createElement("li");
+            taskItem.classList.add("task-item");
+
+            let checkedAttribute = task.completed ? "checked" : "";
+            taskItem.innerHTML = `
+                <input type="checkbox" onclick="postCheckedTask(${task.id}, this)" ${checkedAttribute} id="${task.id}">
+                <strong>${task.title}</strong> ${task.description}
+                <img src="../../src/assets/edit.png" class="edit-image" onclick="editTask(${task.id})" id="${task.id}"/>
+                <img src="../../src/assets/garbage.png" class="delete-image" onclick="deleteTask(${task.id})" id="${task.id}"/>
+            `;
+
+            taskList.appendChild(taskItem);
+        });
+    });
 });
 
 async function postCheckedTask(id, checkbox) {
@@ -73,23 +139,23 @@ async function postCheckedTask(id, checkbox) {
 
 async function editTask(id) {
     localStorage.setItem("editId", id);
-    window.location.href="../edit.html"
+    window.location.href = "../edit.html";
 }
 
 async function deleteTask(id) {
     Swal.fire({
-        title: 'Task will be deleted. Are you sure?',
+        title: "Task will be deleted. Are you sure?",
         showDenyButton: true,
-        confirmButtonText: 'Delete',
+        confirmButtonText: "Delete",
         denyButtonText: `Don't delete`,
-      }).then((result) => {
+    }).then(async (result) => {
         if (result.isConfirmed) {
-          let result = fetch(`http://localhost:3000/tasks/${id}`, {
-            method: "DELETE",
-        });
-        let data = result.json();
+            await fetch(`http://localhost:3000/tasks/${id}`, {
+                method: "DELETE",
+            });
+            showTasksInScreen(); // Refresh task list after deletion
         } else if (result.isDenied) {
-          Swal.fire('Task has not been deleted', '', 'info')
+            Swal.fire("Task has not been deleted", "", "info");
         }
-      })
+    });
 }
